7220a7f5aca663c79c8dd0379383f6e78ccd1e27
Javier Gajardo <jgajardo@gmail.com>
2025-04-20 11:09:16 -0400

    Carga Nuevos Tests

--- app_ppda/tests.py
+++ app_ppda/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+# Test del endpoint ping
+class HealthyTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_healthy_get(self):
+        response = self.client.get('/api/healthy/')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"message": "Estoy aqui"})
+
+    def test_healthy_post(self):
+        response = self.client.post('/api/healthy/')
+        self.assertEqual(response.status_code, 201)
+        self.assertJSONEqual(response.content, {"message": "Estoy aqui"})
+
+class AllTests(TestCase):
+    def setUp(self):
+        self.client= Client()
+    def test_AllFail(self):
+        response =self.client.get('/api/all/')
+        self.assertEqual(response.status_code,401)
