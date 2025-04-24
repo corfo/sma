76dbae6d576bce665893f5c828035da5c3c9b106
Javier Gajardo <jgajardo@gmail.com>
2025-04-23 21:45:57 -0400

    Modificaciones para prueba CI/CD

--- poject_sma/settings.py
+++ poject_sma/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY","DEFAULT_SECRET_KEY_PRO")
+SECRET_KEY = os.environ.get("SECRET_KEY","MyKey_Defaultsadsa973574534rweSDSAFRF324324324.,")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
